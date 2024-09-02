93e78c8fa35ce623e4d0217616f3747b5060ba9b
kyle2501@gmail.com <kyle2501@gmail.com>
2024-09-02 15:32:11 -0700

    update

---
+++ main.go
@@ -0,0 +1,114 @@
+ // # ~ . ~
+// ,  ° . +
+package main
+
+import (
+    "os"
+    "log"
+		
+	"text/template"
+	"net/http"
+	
+//	"time"
+
+)
+
+// ,  ° . +
+type htmlPageData struct {
+    pageTitle string
+    pagePath string
+    pageList []pageNav
+    
+}
+
+type pageNav struct {
+    pageTitle string
+    pageLink string
+}
+
+type pageList struct {
+    pageTitle string
+    pageLink string
+}
+
+
+// ,  ° . +
+func app_welcome_center_page() {
+
+
+}
+
+
+// . indexHandler,  ~ for Public Pages °
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+// ,  ° . +
+    if r.URL.Path != "/" {
+    	http.NotFound(w, r)
+    	return
+    }
+
+// , ° . +
+  pageTitle := "~ . - // - Website App"
+  pagePath := r.URL.Path
+  // pageType := ".."
+
+
+// ,  ° . +
+pageData := htmlPageData {
+      pageTitle: pageTitle,
+      pagePath: pagePath,
+      
+    //  pageList: []pageNav {
+  //        { pageTitle: "one", pageLink: "one"},
+//          { pageTitle: "two", pageLink: "two"},
+   //       { pageTitle: "three", pageLink: "three"},
+    //  },
+  	
+  }  //. .  pageData
+  
+  
+  if pagePath == "/" {
+      pageTitle = "Index Page"
+ //     pageList = pageList
+  }
+
+
+// ,  ° . +
+  pageFilePath := template.Must(template.ParseFiles("layout_main_page.html"))
+  pageFilePath.Execute(w, pageData)
+  
+}  //  .  indexHandler
+
+
+
+
+//  .  html url routes 
+//  .  as well as terminal cli logs
+
+func main() {
+// ,  ° . +
+  appName := "~ . - // - Website App"
+
+// ,  ° . +
+  http.HandleFunc("/", indexHandler)
+
+
+
+// -- -
+  port := os.Getenv("PORT")
+  if port == "" {
+    port = "8080"
+    log.Printf("Loading _webapp with default port")
+  }
+  
+// ,  ° . +
+  log.Printf("_webapp is active and Listening on port %s", port)
+
+  log.Printf("// -- - %s", appName)
+  log.Printf("_webapp now loaded and running at http://localhost:%s", port)
+
+// -- - 
+  if err := http.ListenAndServe(":"+port, nil); err != nil {
+    log.Fatal("Error Starting the HTTP Server :", err)
+    return
+  }}
