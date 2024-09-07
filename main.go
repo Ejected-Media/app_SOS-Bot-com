88929400da586c77f33876c230bf656e39b5c403
kyle2501@gmail.com <kyle2501@gmail.com>
2024-09-07 15:36:07 -0700

    update main.go

--- main.go
+++ main.go
@@ -34,6 +34,21 @@ type pageList struct {
 }
 
 
+type Todo struct {
+    Title string
+    Done  bool
+}
+
+
+
+type TodoPageData struct {
+    PageTitle string
+    Todos     []Todo
+}
+
+
+
+
 // ,  ° . +
 func app_welcome_center_page() {
 
@@ -134,19 +149,30 @@ func hello(w http.ResponseWriter, r *http.Request) {
 //	pagePath := r.URL.Path
     fmt.Fprintf(w, "Hello!")
 }
+
+
 func world(w http.ResponseWriter, r *http.Request) {
 	pagePath := r.URL.Path
-    fmt.Fprintf(w, "World!")
-    fmt.Fprintf(w, pagePath)
+   // fmt.Fprintf(w, "World!")
+   // fmt.Fprintf(w, pagePath)
     
     pageData := pagePath
+    
+    data := TodoPageData{
+            PageTitle: "My TODO list",
+            Todos: []Todo{
+                {Title: "Task 1", Done: false},
+                {Title: "Task 2", Done: true},
+                {Title: "Task 3", Done: true},
+            },
+        }
  
  pageHTML := "layout_main_page.html";
  
   // ,  ° . +
   pageFilePath := template.Must(
     template.ParseFiles(pageHTML))
-  pageFilePath.Execute(w, pageData)
+  pageFilePath.Execute(w, data)
   
   
 }
