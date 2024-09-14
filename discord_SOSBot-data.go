af53a7c951c0e08ff9a6a2972edf08a5c8010b5d
kyle2501@gmail.com <kyle2501@gmail.com>
2024-09-14 07:53:46 -0700

    update discord_SOSBot-data.go

---
+++ discord_SOSBot-data.go
@@ -0,0 +1,184 @@
+<!doctype html>
+<html>
+	
+<body>
+<div class="top_bar_wrap">
+	
+</div><!-- - . top_bar_wrap - -->
+	
+<main>
+	<p>! - SOS-Bot.com .. for // Learn.gallery/chat *</p>
+	
+	<div class="navList_wrap">
+		<nav><p><ul>
+			<li>. // Welcome Zone</li>
+			<li>. // Main Hallway</li>
+			<li>. // Begining Setup</li>
+			<li>. // Support Help</li>
+			<li>. // Office Department</li>
+			<li>. // Bot Channels</li>
+			<li>. // Library Study</li>
+			<li>. // Lounge Lobby</li>
+			<li>. // Recess Quad</li>
+			<li>. // Team Groups</li>
+			<li>. // Voice Channels</li>
+			<li>. // Software & UX Course</li>
+			<li>. // CSS Layout Course</li>
+			<li>. // DNS Server Course</li>
+			<li>. // JS Code Course</li>
+			
+		</ul></p></nav>
+	</div><!-- - . navList_wrap - -->
+		
+		
+	<div class="roomList_wrap">
+		<div>
+			<div><b>. // Welcome Zone</b>
+<div><p><ul>
+	<li>. // Welcome Board</li>
+	<li>. // Information</li>
+	<li>. // Begining Setup</li>
+	<li>. // Waiting Room</li>
+	<li>. // Leaving Room</li>
+</ul></p></div>
+
+</div>
+			<div><b>. // Main Hallway</b>
+<div><p><ul>
+	<li>. // General</li>
+	<li>. // Metting Room</li>
+	<li>. // CSS Layout</li>
+	<li>. // DNS Server</li>
+	<li>. // Software UX</li>
+</ul></p></div>
+
+</div>
+			<div><b>. // Begining Setup</b>
+<div><p><ul>
+  <li>. // Setup Intro</li>
+  <li>. // Profile Setup</li>
+  <li>. // Discord Setup</li>
+  <li>. // Assign Roles</li>
+  <li>. // Roles ASL</li>
+  <li>. // System Specs</li>
+</ul></p></div>
+
+</div>
+			<div><b>. // Support Help</b>
+<div><p><ul>
+	<li>. // Feedback Room</li>
+	<li>. // Issue Tickets</li>
+	<li>. // Issue Venting</li>
+	</ul></p></div>
+
+</div>
+			<div><b>. // Office Department</b>
+<div><p><ul>
+	<li>. // Channel Index List</li>
+	<li>. // Front Desk</li>
+	<li>. // Paperwork Files</li>
+	<li>. // Server Rules</li>
+	<li>. // Join Course</li>
+	</ul></p></div>
+</div>
+			<div><b>. // Bot Channels</b>
+<div><p><ul>
+	<li>. // Bot Test Area</li>
+	<li>. // Bot Logs</li>
+	<li>. // Bot Spam</li>
+	</ul></p></div>
+</div>
+			<div><b>. // Library Study</b>
+<div><p><ul>
+	<li>. // Article Links</li>
+	<li>. // Glossary Items</li>
+	<li>. // Homework</li>
+	</ul></p></div>
+</div>
+<div><b>. // Lounge Lobby</b>
+<div><p><ul>
+  <li>. // Lounge Lobby</li>
+  <li>. // Introductions</li>
+  <li>. // Student Selfies</li>
+  <li>. // Computer Photos</li>
+  <li>. // Desktop Screens</li>
+  <li>. // Pet Photos</li>
+</ul></p></div>
+</div>
+
+
+			<div><b>. // Recess Quad</b>
+<div><p><ul>
+  <li>. // Hangout Area</li>
+  <li>. // Playground Park</li>
+  <li>. // Nap Time</li>
+</ul></p></div>
+</div>
+			<div><b>. // Team Groups</b>
+<div><p><ul>
+	<li>. // Team Groups</li>
+	<li>. // Project Join</li>
+	<li>. // Brainstorm Center</li>
+	</ul></p></div>
+</div>
+			<div><b>. // Voice Channels</b>
+<div><p><ul>
+	<li>.  // -</li>
+	</ul></p></div>
+</div>
+			<div><b>. // Software & UX Course</b>
+<div><p><ul>
+  <li>. // Welcome Group</li>
+  <li>. // Elements for User Interface</li>
+  <li>. // Art and Design Principles</li>
+  <li>. // Wireframe Sketch Mockups Prototypes</li>
+  <li>. // Design System Methodology</li>
+  <li>. // Portfolio Jobs Careers Companies</li>
+</ul></p></div>
+</div>
+			<div><b>. // CSS Layout Course</b>
+<div><p><ul>
+	<li>. // Welcome Group</li>
+	<li>. // Learn The Basics</li>
+	<li>. // Explore The Box Model</li>
+	<li>. // Four Letter Names</li>
+	<li>. // Build Something</li>
+	<li>. // Presentation Skills</li>
+	<li>. // On Going Development</li>
+	</ul></p></div>
+
+</div>
+			<div><b>. // DNS Server Course</b>
+<div><p><ul>
+  <li>. // Welcome Group</li>
+  <li>. // Learn The Basics</li>
+  <li>. // Client Server Model</li>
+  <li>. // Data Types</li>
+  <li>. // Data Manipulation</li>
+  <li>. // User Publish Manage</li>
+</ul></p></div>
+</div>
+			<div><b>. // JS Code Course</b>
+<div><p><ul>
+  <li>. // Syntax</li>
+  <li>. // Vocabulary</li>
+  <li>. // Data Types</li>
+  <li>. // Functions</li>
+  <li>. // Server Calls</li>
+  <li>. // DOM Processing</li>
+
+</ul></p></div>
+</div>
+			
+		</div>
+	</div><!-- - . roomList_wrap - -->
+	
+</main>
+
+<footer>
+	
+	
+</footer>
+
+</body>
+</html>
